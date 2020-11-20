7ae92517f583213d1b732724c68288453c826717
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-20 03:25:57 -0800

    html,css,js

---
+++ donut.js
@@ -0,0 +1,37 @@
+const flavors = document.getElementById('flavors');
+const toppings = document.getElementById('toppings');
+const  fruit = document.getElementById('fruit');
+const donutContainer = document.getElementById("result-container");
+const Donutbutton = document.getElementById("result-container");
+
+
+Donutbutton.addEventListener('click', () => {
+
+});
+
+
+var flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
+console.log(flavors[0]);
+
+var flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
+console.log(flavors[0]);
+    .onclick = function() {myFunction()};
+
+    const flavors = document.getElementById("generate")[
+     fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach"]
+    topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"]
+    
+    function dnutGenerator() {
+        for (i = 0 ; i <flavors.length; i++ ){
+            console.log(flavors);
+        }
+      fruit.forEach(generate => {
+          console.log(fruit);
+          flavors
+
+          topping.forEach(generate => {
+          console.log(topping);
+      });
+    }
+
+
