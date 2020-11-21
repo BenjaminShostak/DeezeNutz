9b7395daf519383ccfb9d4ad33146b1f7c28a9f2
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-21 00:24:46 -0800

    adding js

--- donut.js
+++ donut.js
@@ -1,37 +1,33 @@
-const flavors = document.getElementById('flavors');
-const toppings = document.getElementById('toppings');
-const  fruit = document.getElementById('fruit');
-const donutContainer = document.getElementById("result-container");
-const Donutbutton = document.getElementById("result-container");
+
+const generate = document.getElementById('generate');
 
 
-Donutbutton.addEventListener('click', () => {
+const flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
+const fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach"]
+const  topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"]
 
-});
 
+function DonutGenerator(){
 
-var flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
-console.log(flavors[0]);
-
-var flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
-console.log(flavors[0]);
-    .onclick = function() {myFunction()};
-
-    const flavors = document.getElementById("generate")[
-     fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach"]
-    topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"]
-    
-    function dnutGenerator() {
-        for (i = 0 ; i <flavors.length; i++ ){
-            console.log(flavors);
-        }
-      fruit.forEach(generate => {
-          console.log(fruit);
-          flavors
-
-          topping.forEach(generate => {
-          console.log(topping);
-      });
+    for (i = 0 ; i <flavors.length; i++ ){
+        console.log(i);
     }
+  fruit.forEach(generate => {
+      console.log(fruit);
+      flavors
+
+      topping.forEach(generate => {
+      console.log(topping);
+     dAnswer = ("How about " + flavors + fruit + topping);
+  });
 
 
+  DonutGenerator
+      // Event setup using a convenience method
+$( "result-container" ).click(function DonutGenerator() {
+    console.log(dAnswer);
+    return dAnswer;
+  }
+
+
+}
