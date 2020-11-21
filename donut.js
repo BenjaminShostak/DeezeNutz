884b6dd564f57579d894c438003fe9087737a4ec
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-21 09:59:55 -0800

    updated JS/jquiery

--- donut.js
+++ donut.js
@@ -1,33 +1,34 @@
+$(document).ready(function(){
+const generate = document.getElementById('dnt-btn');
 
-const generate = document.getElementById('generate');
 
-
-const flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
-const fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach"]
-const  topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"]
+const flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ];
+const fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach", "orange","lemon", "lime"];
+const  topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"];
 
 
 function DonutGenerator(){
 
     for (i = 0 ; i <flavors.length; i++ ){
         console.log(i);
-    }
-  fruit.forEach(generate => {
-      console.log(fruit);
-      flavors
-
-      topping.forEach(generate => {
-      console.log(topping);
-     dAnswer = ("How about " + flavors + fruit + topping);
-  });
-
-
-  DonutGenerator
-      // Event setup using a convenience method
-$( "result-container" ).click(function DonutGenerator() {
-    console.log(dAnswer);
-    return dAnswer;
-  }
-
-
+    };
 }
+//   fruit.forEach(generate => {
+//       console.log(fruit);
+      
+
+//       topping.forEach(generate => {
+//       console.log(topping);
+//      dAnswer = alert("How about " + flavors + fruit + topping);
+     
+//   });
+
+      // Event setup using a convenience method
+//$( `.dnt-btn` ).click(function DonutGenerator() {
+  //  console.log(dAnswer);
+    //return $(`result-container`).append(dAnswer);
+//}
+
+
+$('#dnt-btn-btn').click(DonutGenerator);
+});
---
+++ scratchjavscr.js
@@ -0,0 +1,45 @@
+// const flavors = document.getElementById('flavors');
+// const toppings = document.getElementById('toppings');
+// const  fruit = document.getElementById('fruit');
+// const donutContainer = document.getElementById("result-container");
+// const Donutbutton = document.getElementById("result-container");
+// var flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ]
+//  var    fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach"]
+//   var  topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"]
+//   var dAnswerLength= flavors + fruit + topping  
+   
+
+
+// Donutbutton.addEventListener('click', () => {
+// // Event setup using a convenience method
+// $( "result-container" ).click(function() {
+//     console.log(dAnswerLength);
+//     alert(this.fruit + this.flavors + this.topping)
+// });
+// });
+
+
+// alert(this.fruit + this.flavors + this.topping
+
+  
+//    const dAnswer = new Map([[flavors], [fruit], [topping]]);
+  
+//   for (const [key, value] of dAnswer) {
+//     console.log{`${key}'s chart position is ${value}`};
+//   }
+
+
+//     /*function dnutGenerator() {
+//         for (i = 0 ; i <flavors.length; i++ ){
+//             console.log(flavors);
+//         }
+//       fruit.forEach(generate => {
+//           console.log(fruit);
+//           flavors
+
+// //           topping.forEach(generate => {
+//           console.log(topping);
+//       });
+//     }
+
+
