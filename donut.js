0be1dd67db0e9b1820374e5c7f61534d1aabd3c3
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-21 21:42:45 -0800

    just updating for fun

--- donut.js
+++ donut.js
@@ -2,19 +2,42 @@ $(document).ready(function(){
 const generate = document.getElementById('dnt-btn');
 
 
-const flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate" ];
-const fruit = ["apple", "bananna", "strawberry", "blueberry", "raspberry ", "mango", "peach", "orange","lemon", "lime"];
+const flavors = ["chocolate", "vanilla", "Caramel", "butterscotch", "maple", "white chocolate","lemon", "lime" ];
+const fruit = ["apple", "banana", "strawberry", "blueberry", "raspberry ", "mango", "peach", "orange", "grape fruit", "blackberry", "coffee"];
 const  topping = [ "cookie crumb", "chocolate chips","praline", "graham cracker", "coconut"];
 
 
 function DonutGenerator(){
+    
 
     for (i = 0 ; i <flavors.length; i++ ){
         console.log(i);
     };
+
+var myflavor = Math.random()*flavors.length;
+    console.log(myflavor);
+
+    console.log(Math.floor(myflavor));
+
+    console.log(flavors[Math.floor(myflavor)]);
+    $('#result').text(flavors[Math.floor(myflavor)]);
+
+  
+
+    // for ( i = 0 ; 1 <fruit.length; i++ ){
+    // console.log(i);
+    
+    return
+    
+};
+
+function (){
+    for (i = 0 ; i <flavors.length; i++ ){
+        console.log(i);
+    };
 }
-//   fruit.forEach(generate => {
-//       console.log(fruit);
+}
+  
       
 
 //       topping.forEach(generate => {
